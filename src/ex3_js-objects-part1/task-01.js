b38c9728c5f63263051bcd5b5ef126d6482ac595
Anastasia <ansalynova@gmail.com>
2022-03-12 15:26:54 +0300

    Complete task 01

--- src/ex3_js-objects-part1/task-01.js
+++ src/ex3_js-objects-part1/task-01.js
@@ -1,10 +1,5 @@
-function createObject() {
-  const newObject = {};
-  newObject.valueNum = 1;
-  newObject.valueStr = 'hello';
-  newObject.valueBool = true;
-  delete newObject.valueStr;
-  return newObject;
-}
-
-module.exports = createObject;
+const newObject = {};
+newObject.valueNum = 1;
+newObject.valueStr = 'hello';
+newObject.valueBool = true;
+delete newObject.valueStr;
