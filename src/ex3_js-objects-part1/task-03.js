722f265c5d8c3d1be16283ab5f8b2a4513d94d75
Anastasia <ansalynova@gmail.com>
2022-03-12 11:39:47 +0300

    Complete task 05

--- src/ex3_js-objects-part1/task-03.js
+++ src/ex3_js-objects-part1/task-03.js
@@ -1,5 +1,9 @@
 function checkProperty(propertyName, objectName) {
-  return (propertyName in objectName);
+  let exists = false;
+  if (objectName.hasOwnProperty(propertyName)) {
+    exists = true;
+  }
+  return exists;
 }
 
 module.exports = checkProperty;
