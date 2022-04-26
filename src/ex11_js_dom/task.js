6811561dc91fec7e548984f7d698765e506f3274
Anastasia <ansalynova@gmail.com>
2022-04-26 19:41:22 +0400

    Add dynamic image setup

--- src/ex11_js_dom/task.js
+++ src/ex11_js_dom/task.js
@@ -1,3 +1,22 @@
+function setupImages() {
+  const setup = ['asset/cave.jpg', 'asset/forest.jpg', 'asset/lake.jpg', 'asset/sea.jpg', 'asset/snow.jpg'];
+  const alt = ['cave', 'forest', 'lake', 'sea', 'snow'];
+  for (let index = 0; index < setup.length; index += 1) {
+    const newImage = document.createElement('img');
+    newImage.classList.add('slider-img');
+    newImage.setAttribute('src', setup[index]);
+    newImage.setAttribute('alt', alt[index]);
+    if (index === 0) {
+      newImage.classList.add('current');
+    }
+    document.getElementById('slider-img-wrp').appendChild(newImage);
+  }
+
+  return 0;
+}
+
+setupImages();
+
 const images = document.getElementsByClassName('slider-img');
 const dots = document.getElementsByClassName('nav-dot');
 let imageCurrent = 0;
